337052cd02b4fe5d94713eb2d8c3fb950463581c
JC <jchan48@gmail.com>
2023-03-12 00:23:05 -0500

    updated formatting

--- notebook_docs/print_yaml_utils.py
+++ notebook_docs/print_yaml_utils.py
@@ -1,5 +1,7 @@
 import yaml
 from IPython.display import Markdown, display
+
+
 def print_md(string):
     display(Markdown(string))
 
@@ -13,7 +15,7 @@ def print_yaml_md(yaml_dump_data):
 
 
 def print_dict_as_yaml_md(data_dict):
-    print_yaml_md(yaml.dump(data_dict, default_flow_style=None,sort_keys=False))  # maybe want to return as well..
+    print_yaml_md(yaml.dump(data_dict, default_flow_style=None, sort_keys=False))  # maybe want to return as well..
     # print(yaml.dump(data[0]["apply"][0], default_flow_style=False)) # looks the same
     # print(yaml.dump(data[0]["apply"][0], default_flow_style=None )) # Compressed objects - recommended one
     # print(yaml.dump(data[0]["apply"][0], default_flow_style=True )) # very compressed. both list and obj
