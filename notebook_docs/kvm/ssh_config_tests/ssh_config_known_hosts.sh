1d2f6880f84f65aced8c7aaab391685532a04628
JC <jchan48@gmail.com>
2023-03-19 16:56:09 -0400

    ssh config for windows done

--- notebook_docs/kvm/ssh_config_tests/ssh_config_known_hosts.sh
+++ notebook_docs/kvm/ssh_config_tests/ssh_config_known_hosts.sh
@@ -8,9 +8,14 @@ echo `date`
 rm -f ~/.ssh/known_hosts.*
 ls ~/.ssh/
 # ssh -F know_hosts.ssh.config root@192.168.122.1
+# found key in known_hosts....
+# ps1 - windows has to use ed key
+# ssh-keygen -t ed25519
 ssh -F /root/make_hal9000/ssh_config_tests/know_hosts.ssh.config root@localhost 'hostname'
 ssh -F /root/make_hal9000/ssh_config_tests/know_hosts.ssh.config root@192.168.122.1 'hostname'
 ssh -F /root/make_hal9000/ssh_config_tests/know_hosts.ssh.config jason@192.168.122.77 'hostname'
 
 # ssh -F /root/make_hal9000/ssh_config_tests/know_hosts.ssh.config root@127.0.0.1 'hostname' # this doesnt work
 # host key verification failed?
+#  ssh -vv localhost
+# windows
