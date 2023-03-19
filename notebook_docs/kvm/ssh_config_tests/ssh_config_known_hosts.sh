800c6d55d6ea9e1fdf549af0a3b27fa6748c3083
JC <jchan48@gmail.com>
2023-03-19 16:39:06 -0400

    added notes for ssh config

---
+++ notebook_docs/kvm/ssh_config_tests/ssh_config_known_hosts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# need to change config
+# https://linux.die.net/man/1/ssh
+# https://linux.die.net/man/5/ssh_config
+# ssh -F configfile 
+clear
+echo `date`
+rm -f ~/.ssh/known_hosts.*
+ls ~/.ssh/
+# ssh -F know_hosts.ssh.config root@192.168.122.1
+ssh -F /root/make_hal9000/ssh_config_tests/know_hosts.ssh.config root@localhost 'hostname'
+ssh -F /root/make_hal9000/ssh_config_tests/know_hosts.ssh.config root@192.168.122.1 'hostname'
+ssh -F /root/make_hal9000/ssh_config_tests/know_hosts.ssh.config jason@192.168.122.77 'hostname'
+
+# ssh -F /root/make_hal9000/ssh_config_tests/know_hosts.ssh.config root@127.0.0.1 'hostname' # this doesnt work
+# host key verification failed?
